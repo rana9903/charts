50d6c4e9ce3e1925372fb3ce125e7c01e3c07803
Mohiuddin Rana <rana.ca@gmail.com>
2018-05-27 12:04:30 -0400

    Update dataFrame.py

--- dataFrame.py
+++ dataFrame.py
@@ -1,6 +1,13 @@
         data = [['Alex',10],['Bob',12],['Clarke',13], ['Rana', 60]]
         df = pd.DataFrame(data,columns=['Firstname','Age'], index = ['rank1','rank2','rank3', 'rank4'], dtype = float)
         print df
+/*        
+           Firstname   Age
+rank1      Alex  10.0
+rank2       Bob  12.0
+rank3    Clarke  13.0
+rank4      Rana  60.0
+/*        
         print "How to add new rows"
         data = [['Alexa',10]]
         df1 = pd.DataFrame (data,columns=['Firstname','Age'], index = ['rank5'] )
