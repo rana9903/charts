9ab6d2ba6249aed3b445407b948f68805656c4df
Mohiuddin Rana <rana.ca@gmail.com>
2018-05-27 12:23:08 -0400

    Update dataFrametutorials.py

--- dataFrametutorials.py
+++ dataFrametutorials.py
@@ -1,4 +1,8 @@
-        data = [['Alex',10],['Bob',12],['Clarke',13], ['Rana', 60]]
+"""
+Panda dataframe is 
+"""
+
+data = [['Alex',10],['Bob',12],['Clarke',13], ['Rana', 60]]
         df = pd.DataFrame(data,columns=['Firstname','Age'], index = ['rank1','rank2','rank3', 'rank4'], dtype = float)
         print df
         
@@ -39,3 +43,28 @@ rank4      Rana  60.0     11
 rank5     Alexa  10.0      3
 
 """
+print "Reversing dataframe"
+df =df[::-1]
+print df 
+
+"""
+Its output is as follows −
+Reversing dataframe
+      Firstname   Age  phone
+rank5     Alexa  10.0      3
+rank4      Rana  60.0     11
+rank3    Clarke  13.0     16
+rank2       Bob  12.0     15
+rank1      Alex  10.0     10
+
+"""
+
+
+""""
+https://www.tutorialspoint.com/python_pandas/python_pandas_dataframe.htm
+https://stackoverflow.com/questions/12555323/adding-new-column-to-existing-dataframe-in-python-pandas
+
+""""
+
+
+
